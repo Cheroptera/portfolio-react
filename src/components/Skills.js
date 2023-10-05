fd0055736c07367a9f0a604de428cb34f8338435
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-10-05 11:54:48 +0200

    updated skills-list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,16 +29,20 @@ const Skills = () => {
             <li>Postman</li>
             <li>Compass</li>
             <li>Atlas</li>
+            <li>Notion</li>
             <li>Adobe Photoshop</li>
             <li>Adobe InDesign</li>
             <li>Pro Tools</li>
           </ul>
           <ul className="skills-list" id="moreList">
             <li> <span className="more"> More </span></li>
+            <li>Music Production</li>
             <li>Marketing</li>
+            <li>Entrepreneurship</li>
             <li>Social Media</li>
             <li>Copywriting</li>
             <li>Design</li>
+            <li>Layouts</li>
             <li>Teaching</li>
             <li>Translation</li>
             <li>Sales</li>
@@ -46,11 +50,11 @@ const Skills = () => {
           </ul>
           <ul className="skills-list" id="upcomingList">
             <li> <span className="upcoming"> Upcoming </span></li>
-            <li>React Native</li>
             <li>Next.js</li>
             <li>TypeScript</li>
             <li>SASS/SCSS</li>
             <li>Sanity</li>
+            <li>React Native</li>
           </ul>
         </section>
       </div>
