f0f31b177aeb239d2235fd7aeca2a4c79d9eda38
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-12-20 21:15:33 +0100

    added new project and added text

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,7 +4,7 @@ export const projectList = [
     title: 'Circuit Training App',
     img: './images/circle-it-cover.webp',
     alt: 'picture of workout app',
-    description: "Fullstack project, in which we've built our own API and database. Login functionality that unlocks more features for the user.",
+    description: 'Fullstack team project, in which we built our own API and database. Login functionality that unlocks more features for the user such as workout randomizer, filtering function, favorite function, save function, see recent workouts and more.',
     tags: ['Figma', 'Styled Components', 'JavaScript', 'API', 'React', 'MongoDB', 'Express.js', 'Node.js'],
     demo: 'https://circle-it.netlify.app/',
     hasBackendCode: true,
@@ -17,7 +17,7 @@ export const projectList = [
     title: 'Happy Vibes App',
     img: './images/project-happy-thoughts.webp',
     alt: 'picture of computer',
-    description: 'A Twitter-like app without all the negativity. Fullstack project.',
+    description: 'A Twitter-like app without all the negativity. A fullstack project in which I stored the fetched data from the API into the state. I worked with React hooks and used useEffect to fetch data from the API. I also built my own API using Express and MongoDB.',
     tags: ['HTML', 'CSS', 'JavaScript', 'API', 'React', 'Express.js', 'MongoDB', 'Node.js'],
     demo: 'https://master--jovial-hamster-0bbfb9.netlify.app/',
     hasBackendCode: true,
@@ -27,10 +27,23 @@ export const projectList = [
     }
   },
   {
+    title: 'News Article App - Case Study',
+    img: './images/simple-news-app.webp',
+    alt: 'picture of computer',
+    description: 'A simple news article app, built in React. The app fetches Mockdata from an API that I built using JSON-server. Features include: pagination, handling of paid content, article navigation and more.',
+    tags: ['HTML', 'CSS', 'JavaScript', 'API', 'React', 'Axios', 'Node.js'],
+    demo: 'https://golden-sunburst-758208.netlify.app/',
+    hasBackendCode: true,
+    code: {
+      frontend: 'https://github.com/Cheroptera/article-page',
+      backend: 'https://json-server-news-site.onrender.com'
+    }
+  },
+  {
     title: 'To-do App',
     img: './images/project-todos.webp',
     alt: 'picture of computer',
-    description: 'To-do app built in React, using Redux to write my own reducers and actions.',
+    description: 'To-do app built in React. I worked with Redux Toolkit and used Styled Components for styling. I created slices, reducers and actions to handle the state.',
     tags: ['JavaScript', 'Redux', 'React', 'Styled Components'],
     demo: 'https://getitdonealready.netlify.app',
     hasBackendCode: false,
@@ -40,18 +53,18 @@ export const projectList = [
     title: 'Quiz App',
     img: './images/project-quiz.webp',
     alt: 'picture of computer',
-    description: 'Group project, in which we used Figma as a planning tool for components and design. The app was built mob-programming.',
+    description: 'Group project, in which we used Figma as a planning tool for components and design. We learnt how to work with global state and used selectors to access the Redux state and dispatch to trigger Redux actions.',
     tags: ['Styled Components', 'JavaScript', 'Redux', 'React'],
     demo: 'https://gothenburgerquiz.netlify.app/',
     hasBackendCode: false,
     code: 'https://github.com/Cheroptera/project-redux-quiz'
-
   },
   {
     title: 'Design Handoff',
     img: './images/project-design-handoff.webp',
     alt: 'picture of computer',
-    description: "Transforming a UX-design student's design into code. (Visual design only - no functionality)",
+    description: 'This project involved converting a UX designer\'s vision into tangible code. It was an opportunity to enhance my understanding of the critical role that color schemes, typography, and structural hierarchy play in creating an effective web design.',
+    footnote: 'Please note that this project focused solely on the visual aspects of the design, without incorporating any functional elements.',
     tags: ['HTML', 'CSS'],
     demo: 'https://clever-crostata-bca231.netlify.app/',
     hasBackendCode: false,
@@ -79,7 +92,7 @@ export const projectList = [
   },
   {
     title: 'Music Release App',
-    img: './images/project-music-releases.webp',
+    img: './images/project-music-releases2.webp',
     alt: 'picture of music app',
     description: 'A Music Release App built in React, using components and passing props.',
     tags: ['HTML', 'CSS', 'JavaScript', 'API', 'React', 'Pair-Programming'],
@@ -96,7 +109,6 @@ export const projectList = [
     demo: 'https://theknockknockmachine.netlify.app/',
     hasBackendCode: false,
     code: 'https://github.com/Cheroptera/project-chatbot'
-
   },
   {
     title: 'Weather App',
@@ -109,8 +121,7 @@ export const projectList = [
     code: 'https://github.com/cpz-crafts/project-weather-app'
   },
   {
-
-    title: 'Guess Who - The Annoying People Version',
+    title: 'Guess Who - The game',
     img: './images/project-guess-who.webp',
     alt: 'picture of a game called Guess who',
     description: "This is my version of Guess Who, built with Vanilla Javascript. All characters are illustrated by me. Good luck. You're going to need it! ;)",
