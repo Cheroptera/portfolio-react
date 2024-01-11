011269b3f149da8f054b54a4e3c8fa2f0d37657f
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2024-01-11 10:52:39 +0100

    Udated tech section

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -8,12 +8,12 @@ const Tech = () => {
         <h3>Tech</h3>
       </div>
       <p className="tech-text">
-        HTML5, CSS3, CSS Grid, Flexbox, <span className="tech-word highlight-1" style={{ backgroundColor: 'white' }}> JavaScript/TypeScript</span>, ES6, JSX,
+        HTML5, CSS3, SASS, Tailwind, <span className="tech-word highlight-1" style={{ backgroundColor: 'white' }}> JavaScript/TypeScript</span>, ES6, JSX,
         <span
           style={{ backgroundColor: 'white' }}
           className="tech-word highlight-2"> React
-        </span>, Redux,  <span className="tech-word highlight-3" style={{ backgroundColor: 'white' }}> Node.js</span>, Mongo DB, Web Accessibility,
-        Mob-Programming, Pair-Programming, Github, REST APIs, Google Cloud,  <span className="tech-word highlight-4" style={{ backgroundColor: 'white' }}>Styled Components</span>, Material UI, Bootstrap, Agile Methodologies.
+        </span>, Next.js, Redux,  <span className="tech-word highlight-3" style={{ backgroundColor: 'white' }}> Node.js</span>, Mongo DB, Web Accessibility,
+        Github, REST APIs, Google Cloud,  <span className="tech-word highlight-4" style={{ backgroundColor: 'white' }}>Styled Components</span>, Material UI, Daisy UI, Bootstrap, Agile Methodologies.
       </p>
     </div>
   )
